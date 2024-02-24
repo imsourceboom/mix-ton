fea6523bc5f136a6cdab5a91ee7cf202063a4bac
imsourceboom <ecmascript.devloper@gmail.com>
2024-02-24 17:11:53 +0900

    update removeWallet script and change name createWallets scripts

--- scripts/removeWallets.sh
+++ scripts/removeWallets.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-rm `ls $HOME/.local/share/mytoncore/wallets/*`
+if [ $# -ne 1 ]; then
+	echo "script need 1 parameter"
+	echo "parameter 1: file name"
+	echo "ex) removeWallets.sh v1*"
+	exit 1
+fi
+
+NAME=$1
+
+rm `ls $HOME/.local/share/mytoncore/wallets/${NAME}`
