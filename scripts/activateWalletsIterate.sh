74b5fd44b3ee8e73314cf72c534ea2a52c7d7d61
imsourceboom <ecmascript.devloper@gmail.com>
2024-02-26 16:31:12 +0900

    update activateWallets scripts

--- scripts/activateWalletsIterate.sh
+++ scripts/activateWalletsIterate.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "script need 2 parameter"
-	echo "parameter 1: name"
-	echo "parameter 2: iterate limit count"
+if [ $# -ne 3 ]; then
+	echo "script need 3 parameter"
+	echo "parameter 1: wallet name"
+	echo "parameter 2: iterate start number"
+	echo "parameter 3: iterate end number"
 	exit 1
 fi
 
 NAME=$1
-COUNT=$2
+START=$2
+END=$3
 
-for ((i = 1; i <= $COUNT; i++))
+for ((i = $START; i <= $END; i++))
 do 
 	activateWallet.sh ${NAME}_${i}
 done
