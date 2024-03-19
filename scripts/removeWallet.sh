5d38613e1b29f0da7897cbb0247a591af687d007
imsourceboom <ecmascript.devloper@gmail.com>
2024-03-19 14:54:04 +0900

    update remove wallet scripts

---
+++ scripts/removeWallet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "script need 1 parameter"
+	echo "parameter 1: file name"
+	echo "ex) removeWallets.sh wallet_name"
+	exit 1
+fi
+
+NAME=$1
+
+rm `ls $HOME/.local/share/mytoncore/wallets/${NAME}-query.boc`
+rm `ls $HOME/.local/share/mytoncore/wallets/${NAME}.addr`
+rm `ls $HOME/.local/share/mytoncore/wallets/${NAME}.pk`
--- scripts/removeWalletsIterate.sh
+++ scripts/removeWalletsIterate.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 3 ]; then
 	echo "script need 3 parameter"
-	echo "parameter 1: wallet name | ex) wallet_name*"
+	echo "parameter 1: wallet name | ex) wallet_name"
 	echo "parameter 2: iterate start number"
 	echo "parameter 3: iterate end number"
 	exit 1
@@ -14,5 +14,5 @@ END=$3
 
 for ((i = $START; i <= $END; i++))
 do
-	removeWallets.sh ${WALLET_NAME}_${i}*
+	removeWallets.sh ${WALLET_NAME}_${i}
 done
